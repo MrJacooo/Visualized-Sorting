a1c6515f61e0628b2169e9860af6b9012437f5bd
MrJacooo <sebastian.storz2003@gmail.com>
2021-08-28 20:48:07 +0200

    Finished Bubblesort without Animation, Finished Bubblesort Stepper without colors

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import logo from './logo.svg';
 import './App.css';
-import react, { useState } from "react"
+import react, { useState, useEffect } from "react"
 import { Button, Form } from "react-bootstrap"
 import { width } from 'dom-helpers';
+import { sort } from 'prelude-ls';
 
 function App() {
   return (
@@ -20,10 +21,16 @@ function NotSuitable() {
   )
 }
 
-function Content() {
 
-  const [bars, setBars] = useState([1])
-  const [barWidth, setBarWidth] = useState(10)
+
+function Content() {
+  const [bars, setBars] = useState([])
+  const [barWidth, setBarWidth] = useState(100)
+  const [sortAlg, setSortAlg] = useState(1)
+  const [sorting, setSorting] = useState(false)
+
+  const [sortStepperOne, setSortStepperOne] = useState(0)
+  const [sortStepperTwo, setSortStepperTwo] = useState(0)
 
 
   function fillBars(num) {
@@ -35,6 +42,8 @@ function Content() {
       tempBars.push(getRandomNumber(window.screen.height / 5 * 3))
     }
     setBars(tempBars)
+    setSortStepperOne(0)
+    setSortStepperTwo(0)
   }
 
   function getRandomNumber(max) {
@@ -42,19 +51,81 @@ function Content() {
   }
 
 
+
+  async function sort() {
+    if (!sorting && bars.length > 0) {
+      setSorting(true)
+      if (sortAlg === 1) {
+        console.log("starting algorythm")
+        let tempArr = bars.slice()
+        for (let i = 0; i < bars.length; i++) {
+
+          //Inner pass
+          for (let j = 0; j < bars.length - i - 1; j++) {
+
+            //Value comparison using ascending order
+
+            if (tempArr[j + 1] < tempArr[j]) {
+              console.log("swapping")
+
+              let temp = tempArr[j];
+              tempArr[j] = tempArr[j + 1];
+              tempArr[j + 1] = temp
+            }
+          }
+        }
+        setBars(tempArr)
+      }
+
+    }
+    setSorting(false)
+  }
+
+  function sortOne() {
+    console.log("starting algorythm")
+    let tempArr = bars.slice()
+    console.log(sortStepperOne, bars.length)
+    if (sortStepperOne < bars.length) {
+
+      console.log(sortStepperTwo, bars.length - sortStepperOne - 1)
+      //Inner pass
+      if (sortStepperTwo < bars.length - sortStepperOne - 1) {
+
+        //Value comparison using ascending order
+        if (tempArr[sortStepperTwo + 1] < tempArr[sortStepperTwo]) {
+
+
+          let temp = tempArr[sortStepperTwo];
+          tempArr[sortStepperTwo] = tempArr[sortStepperTwo + 1];
+          tempArr[sortStepperTwo + 1] = temp
+          setBars(tempArr)
+        }
+        setSortStepperTwo(sortStepperTwo + 1)
+
+      } else { setSortStepperTwo(0); setSortStepperOne(sortStepperOne + 1) }
+    }
+  }
+
+
   return (
     <>
       <header>
         <div>
-          Amount of Objects and Sorting Speed
-          <Form.Range />
+          <Form.Label>Amount of Objects and Sorting Speed</Form.Label>
+          <Form.Range step="1" min="5" max="300" value={bars.length} onChange={(e) => fillBars(e.target.value)} />
         </div>
         <div>
-          <Button onClick={() => fillBars(300)}>Fill bars</Button>
+          <Form.Check id="Radio1" inline type="radio" label="Bubble Sort" name="group1" value={1} checked={sortAlg === 1} onChange={() => setSortAlg(1)} />
+
+          <Form.Check id="Radio2" disabled inline type="radio" label="Sort 2" name="group1" value={1} checked={sortAlg === 2} onChange={() => setSortAlg(2)} />
+        </div>
+        <div>
+          <Button onClick={() => sort()}> Sort 🧩</Button>
+          <Button onClick={() => sortOne()}> Sort one Step</Button>
         </div>
       </header>
       <section>
-        {bars.length > 0 ? bars.map(bar => <div className="bar" style={{ height: `${bar}px`, width: `${barWidth}px` }}></div>) : "no bars"}
+        {bars.length > 0 ? bars.map(bar => <div className="bar" style={{ height: `${bar}px`, width: `${barWidth}px` }}></div>) : <p>Move the Slider at the Top ☝</p>}
       </section>
     </>
   )
